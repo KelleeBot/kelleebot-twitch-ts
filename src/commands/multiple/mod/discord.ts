fad83a4606cfd25b5638052812a1e2b6d70750e3
alima566 <aaronmai@hotmail.com>
2021-12-03 11:45:29 -0700

    added discord command

---
+++ src/commands/multiple/mod/discord.ts
@@ -0,0 +1,15 @@
+import { Command } from "../../../interfaces";
+import discordInvites from "../../../config/discordInvites.json";
+
+export default {
+  name: "discord",
+  category: "Mod",
+  channels: ["ramenbomber_", "jkirstyn"],
+  isModOnly: true,
+  execute({ client, channel }) {
+    const channelName = channel.slice(1).toLowerCase();
+    //@ts-ignore
+    const discord = discordInvites[channelName];
+    return client.say(channel, `/me ${discord}`);
+  }
+} as Command;

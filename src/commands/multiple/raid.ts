d32685093e4a39c88e2fc627158e72e9d05b35d8
alima566 <aaronmai@hotmail.com>
2021-12-08 13:45:53 -0700

    added raid command

---
+++ src/commands/multiple/raid.ts
@@ -0,0 +1,20 @@
+import { Command } from "../../interfaces";
+import { setCooldown } from "../../utils";
+
+const raidMsg = {
+  ramenbomber_: "ramenb2Pichu ramenb2Pichu Ramen Raid!",
+  mackthevoid:
+    "mackth2PillowTime mackth2PillowTime mackth2PillowTime AHHH IT'S A RAID!!! mackth2Fngrgun mackth2Fngrgun mackth2Fngrgun STICK 'EM UP!!!mackth2PillowTime mackth2PillowTime mackth2PillowTime AHHH IT'S A RAID!!! mackth2Fngrgun mackth2Fngrgun mackth2Fngrgun STICK 'EM UP!!!"
+} as { [key: string]: string };
+
+export default {
+  name: "raid",
+  category: "Multiple",
+  cooldown: 15,
+  channels: ["ramenbomber_", "mackthevoid"],
+  execute({ client, channel, userstate }) {
+    setCooldown(client, this, channel, userstate);
+    const channelName = channel.slice(1).toLowerCase();
+    return client.say(channel, `/me ${raidMsg[channelName]}`);
+  }
+} as Command;

a6e1cdc44bb42cb6517ab63e7bbb4ce45a55bad8
Aaron Li-Mai <aaronmai@hotmail.com>
2021-12-09 21:28:48 -0700

    added fc command

---
+++ src/commands/ramenbomber_/fc.ts
@@ -0,0 +1,14 @@
+import { Command } from "../../interfaces";
+import { setCooldown } from "../../utils";
+
+export default {
+    name: "fc",
+    aliases: ["friendcode"],
+    category: "ramenbomber_",
+    channels: ["ramenbomber_"],
+    cooldown: 15,
+    execute({ client, channel, userstate }) {
+        setCooldown(client, this, channel, userstate);
+        return client.say(channel, `/me SW-7337-4574-0022`);
+    }
+} as Command;

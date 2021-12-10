cee3c9804e1ec9ba91a13469654f16dc1abbf423
Aaron Li-Mai <aaronmai@hotmail.com>
2021-12-09 21:19:47 -0700

    added time command

---
+++ src/commands/mackthevoid/time.ts
@@ -0,0 +1,23 @@
+import { Command } from "../../interfaces";
+import { setCooldown } from "../../utils";
+import utc from "dayjs/plugin/utc";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import advanced from "dayjs/plugin/advancedFormat";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(advanced);
+
+export default {
+    name: "time",
+    category: "mackthevoid",
+    channels: ["mackthevoid"],
+    cooldown: 15,
+    execute({ client, channel, userstate }) {
+        setCooldown(client, this, channel, userstate);
+        const time = dayjs()
+            .tz("America_Denver")
+            .format("DD/MM/YYYY h:mm:ss A z");
+        return client.say(channel, `/me MacK's current time is ${time}.`);
+    }
+} as Command;

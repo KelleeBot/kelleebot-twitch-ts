78ace3cf844afe1277516d2a874ab9a6aeaae257
Aaron Li-Mai <aaronmai@hotmail.com>
2021-12-09 22:27:51 -0700

    added raided event

---
+++ src/events/raided.ts
@@ -0,0 +1,29 @@
+import { Client } from "tmi.js";
+import { getCurrentGame } from "../utils";
+import shoutouts from "../config/shoutouts.json";
+
+const autoShoutoutChannels = ["mackthevoid", "ramenbomber_", "jkirstyn"];
+
+export default async (
+    client: Client,
+    channel: string,
+    username: string,
+    viewers: number
+) => {
+    const channelName = channel.slice(1).toLowerCase();
+
+    if (autoShoutoutChannels.includes(channelName)) {
+        client.say(
+            channel,
+            `/me Incoming raid! Thank you @${username} for raiding the channel with ${viewers} viewer${
+                viewers !== 1 ? "s" : ""
+            }! Welcome raiders!`
+        );
+    }
+
+    const game = await getCurrentGame(channel);
+    //@ts-ignore
+    let shoutout = shoutouts[channelName];
+    shoutout = shoutout.replace(/<USER>/g, username).replace(/<GAME>/g, game);
+    setTimeout(() => client.say(channel, `/me ${shoutout}`), 1000 * 5);
+};

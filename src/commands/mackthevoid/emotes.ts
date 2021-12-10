b199149f3721750c1c664753f07918d544e56611
Aaron Li-Mai <aaronmai@hotmail.com>
2021-12-09 20:57:23 -0700

    added emotes command

---
+++ src/commands/mackthevoid/emotes.ts
@@ -0,0 +1,15 @@
+import { Command } from "../../interfaces";
+import { setCooldown } from "../../utils";
+
+export default {
+    name: "emotes",
+    channels: ["mackthevoid"],
+    cooldown: 15,
+    execute({ client, channel, userstate }) {
+        setCooldown(client, this, channel, userstate);
+        return client.say(
+            channel,
+            `/me You like these emotes? mackth2Fngrgun mackth2PillowTime mackth2Love Those stellar sub badges? Look! It's bearyclairey at https://www.twitch.tv/bearyclairey!!! She is the amazing artist who made them so be sure to say thanks by showing your support!`
+        );
+    }
+} as Command;

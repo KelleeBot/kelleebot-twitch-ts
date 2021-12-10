63d8c693e19c19de08377c3c9ce6ebb9bf839020
Aaron Li-Mai <aaronmai@hotmail.com>
2021-12-09 22:18:54 -0700

    added setLink command

---
+++ src/commands/ramenbomber_/mod/setLink.ts
@@ -0,0 +1,26 @@
+import { Command } from "../../../interfaces";
+import { setLink, log, getChannelInfo, errorMessage } from "../../../utils";
+
+export default {
+    name: "setlink",
+    aliases: ["setcode", "setroom", "setroomcode"],
+    category: "Moderation/ramenbomber_",
+    channels: ["ramenbomber_"],
+    isModOnly: true,
+    arguments: [
+        {
+            type: "STRING",
+            prompt: "Please specify a code."
+        }
+    ],
+    async execute({ client, channel, args, userstate }) {
+        const text = args.join(" ");
+        try {
+            await setLink(channel.slice(1), userstate, text);
+            return client.say(channel, `/me The room code has been updated.`);
+        } catch (e) {
+            log("ERROR", `${__filename}`, `An error has occurred: ${e}`);
+            return errorMessage(client, channel);
+        }
+    }
+} as Command;

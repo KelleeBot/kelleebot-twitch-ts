36921e8f9a05bb7bb705c954f02a30abb4b51f0c
Aaron Li-Mai <aaronmai@hotmail.com>
2022-03-24 19:47:16 -0600

    updated ramen raid message

--- src/commands/multiple/raid.ts
+++ src/commands/multiple/raid.ts
@@ -2,7 +2,7 @@ import { Command } from "../../interfaces";
 import { setCooldown } from "../../utils";
 
 const raidMsg = {
-    ramenbomber_: "ramenb2PichYOU The Ramen Rat Pack is here ramenb2PichYOU",
+    ramenbomber_: "ramenb2Pichumper The Ramen Rat Pack is here ramenb2Pichumper",
     mackthevoid:
         "mackth2PillowTime mackth2PillowTime mackth2PillowTime AHHH IT'S A RAID!!! mackth2Fngrgun mackth2Fngrgun mackth2Fngrgun STICK 'EM UP!!!mackth2PillowTime mackth2PillowTime mackth2PillowTime AHHH IT'S A RAID!!! mackth2Fngrgun mackth2Fngrgun mackth2Fngrgun STICK 'EM UP!!!"
 } as { [key: string]: string };
